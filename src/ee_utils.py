cf4d83f161f14e9c382e1166f9727ed35a36153b
ruth <maryruthbongon@gmail.com>
2023-03-14 12:59:08 +0100

    Updated utils file for GEE initialization

--- src/ee_utils.py
+++ src/ee_utils.py
@@ -1,16 +1,23 @@
 import ee
-import sys
+from config import service_email_account, earth_engine_key
 
 
-def image_to_map_id(image_name, vis_params={}):
-    """  """
-    try:
-        ee_image = ee.Image(image_name)
-        map_info = ee_image.getMapId(vis_params)
-        return {
-            'url': map_info['tile_fetcher'].url_format
-        }
-    except Exception as e:
-        return {
-            'errMsg': str(sys.exc_info()[0])
-        }
+# service_account = 'geefire-ruth@geefire-380610.iam.gserviceaccount.com'
+# earth_engine_key = r'src\gee_key.json'
+
+credentials = ee.ServiceAccountCredentials(service_email_account, earth_engine_key)
+ee.Initialize(credentials)
+
+# def image_to_map_id(image_name, vis_params={}):
+#     """  """
+#     try:
+#         ee_image = ee.Image(image_name)
+#         map_info = ee_image.getMapId(vis_params)
+#         return {
+#             'url': map_info['tile_fetcher'].url_format
+#         }
+#     except Exception as e:
+#         return {
+#             'errMsg': str(sys.exc_info()[0])
+#         }
+
