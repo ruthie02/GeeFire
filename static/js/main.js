e57e450cdfe69449e881f1cb21fbe1c826517a56
ruth <maryruthbongon@gmail.com>
2023-03-16 21:04:08 +0100

    Added bootstrap script in html

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
-// blank for now
+/*
+
+What needs to be done here:
+1. Create a map using openlayers
+2. Function to create a Polygon, again with Openlayers
+3. Handling change event
+    a. Date 
+*/
+
+// create an Openlayer Map
