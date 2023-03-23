6a3234e60524862634b93801e7830fa456550ef3
ruth <maryruthbongon@gmail.com>
2023-03-23 21:46:26 +0100

    updated model.py

--- app.py
+++ app.py
@@ -21,29 +21,29 @@ app = Flask(__name__)
 def map():
     return render_template("map.html")
 
-@app.route('/visualize', methods=["ROUTE"])
-def display(request):
-    request_parameters = request.get_json()
+# @app.route('/visualize', methods=["ROUTE"])
+# def display(request):
+#     request_parameters = request.get_json()
 
-    # get the parameters set by the user
+#     # get the parameters set by the user
 
-    x_min, y_min, x_max, y_max = [float(coords) for coords in request_parameters["bbox"].split(",")]
-    pre_start = request_parameters["pre_start"]
-    pre_last = request_parameters["pre_last"]
-    fire_start= request_parameters["fire_start"]
-    fire_last = request_parameters["fire_last"]
-    satellite = request_parameters["satellite"]
+#     x_min, y_min, x_max, y_max = [float(coords) for coords in request_parameters["bbox"].split(",")]
+#     pre_start = request_parameters["pre_start"]
+#     pre_last = request_parameters["pre_last"]
+#     fire_start= request_parameters["fire_start"]
+#     fire_last = request_parameters["fire_last"]
+#     satellite = request_parameters["satellite"]
 
-    # create Google Earth Engine Geometry from the bounding bax parameters
-    ee_geom = ee.Geometry.Rectange([x_min, y_min, x_max, y_max])
+#     # create Google Earth Engine Geometry from the bounding bax parameters
+#     ee_geom = ee.Geometry.Rectange([x_min, y_min, x_max, y_max])
 
-    # create the range dates from the date parameters selected by the user
-    preFire_period = (pre_start, pre_last)
-    postFire_period = (fire_start, fire_last)
+#     # create the range dates from the date parameters selected by the user
+#     preFire_period = (pre_start, pre_last)
+#     postFire_period = (fire_start, fire_last)
 
-    # run the burn severity model 
+#     # run the burn severity model 
 
-    return pre_start
+#     return pre_start
 
 
 
